66c16ef9b8e8908b9783c877913f3ef95a158ed6
+ <+>
2022-06-22 13:27:24 -0600

    commit tree 70c72e1be9c0326670c89a430e7c868f794c3156

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  
+  if array.length > 2
+    new_Array = array[0 , array.length-1]
+    final_array = "#{new_Array.join(", ")}, and #{array.last()}"
+  elsif array.length == 2
+    final_array = "#{array[0]} and #{array[1]}"
+  else 
+    final_array = "#{array[0]}"
+  end
+
+  final_array
 end
+
+
